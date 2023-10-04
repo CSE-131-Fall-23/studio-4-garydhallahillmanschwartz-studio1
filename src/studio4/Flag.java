a50f39bae5cb54f9521b83c9da5aa249ca8c6a2f
asg1217 <asg1217@10.232.200.85>
2023-10-04 14:09:41 -0500

    Finished!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,25 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+	Color purple = new Color(255, 0, 150);
+	StdDraw.setPenColor(purple);
+	StdDraw.filledRectangle(0.5, 0.5, 0.3, 0.2);
+	
+	Color pink = new Color(200, 0, 150);
+	StdDraw.setPenColor(pink);
+	StdDraw.filledRectangle(0.5, 0.5, 0.2, 0.1);
+	
+	Color lightPink = new Color(150, 0, 150);
+	StdDraw.setPenColor(lightPink);
+	StdDraw.filledRectangle(0.5, 0.5, 0.1, 0.05);
+	
+	StdDraw.setPenColor(Color.pink);
+	double[] x = {0.75, 0.75, 0.75};
+	double[] y = {0.75, 0.75, 0.75};
+	StdDraw.filledPolygon(x, y);
+	
+	
+	StdDraw.setPenColor(Color.BLACK);
+	StdDraw.rectangle(0.5, 0.5, 0.3, 0.2);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,7 @@
 package studio4;
 
+import java.awt.Color;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +22,15 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String name = in.nextLine();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		StdDraw.rectangle(x, y, halfWidth, halfHeight);
 	}
 }
